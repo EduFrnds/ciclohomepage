8049e94f2e490bb42ecb84d29ad73fc083a73d59
Eduardo Fernandes <90912186+EduFrnds@users.noreply.github.com>
2026-04-05 21:06:55 -0300

    Add contato section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Page() {
         <PillarsSection />
         <ProjectsSection />
         <ClosingCtaSection />
+        <div id="contato">contato</div>
       </main>
       <Footer />
     </>
